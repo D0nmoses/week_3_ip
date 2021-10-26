0f60252928a00c99070e89d84b20b628679ef82e
Don Moses <githinjidon@gmail.com>
2021-10-26 10:05:52 +0300

    add class Project

--- main/models.py
+++ main/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
 
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     description = models.TextField()
-    image = models.CloudinaryField('photos')
+    image = CloudinaryField('photos')
     project_url = models.URLField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    user_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, default='', null=True)
+
+class Profile(models.Model):
+
