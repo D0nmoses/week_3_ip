d044d8d2579f3d6581f8670c2dcfdb967138cc01
Don Moses <githinjidon@gmail.com>
2021-10-26 09:15:51 +0300

    add cloudinary and main to installed apps

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+
+    title = models.CharField()
+    description = models.TextField()
+    image = models.ImageField
