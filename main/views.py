e672994b403857f79cf87183d63c3338035c8b05
Don Moses <githinjidon@gmail.com>
2021-11-03 20:30:33 +0300

    add serializers.py

--- main/views.py
+++ main/views.py
@@ -87,7 +87,7 @@ def search_results(request):
     
     if 'project' in request.GET and request.GET["project"]:
         search_term = request.GET.get("project")
-        searched_projects = Project.search_by_name(search_term)
+        searched_projects = Project.search_projects(search_term)
         message = f"{search_term}"
 
         return render(request, 'all-project/search.html',{"message":message,"projects": searched_projects})
