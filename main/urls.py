6f0c68bfc6eba5d1b0992df2f077311c510fb956
Don Moses <githinjidon@gmail.com>
2021-11-03 20:22:45 +0300

    add url for search_results

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     re_path(r'^profile/(\d+)', views.profile, name="profile"),
     re_path(r'^create/project', views.new_project, name="new-project"),
     re_path(r'^project/(\d+)', views.project, name="project"),
+    path("search_results", views.search_results, name="search_results"),
 ]
